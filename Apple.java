cd99d8fa7dcd6f09556df222cdc8f58460dc6d5b
JasonLeung1 <440004783@gapps.yrdsb.ca>
2024-09-23 07:33:25 +0800

    Part 1

---
+++ Apple.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Food for our hero.
+ * 
+ * @author Jason Leung  
+ * @version September 2024
+ */
+public class Apple extends Actor
+{
+    /**
+     * Act - do whatever the Apple wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        int x = getX();
+        int y = getY() + 2;
+        setLocation(x, y);
+        
+        MyWorld world = (MyWorld) getWorld();
+        if(getY () >= world.getHeight())
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    public int score = 0;
+    Label scoreLabel;
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,9 +18,45 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1, false);
         
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
+        
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
+        createApple();
     }
-}
+    
+    /**
+     * End the game and 'draw 'GameOver'
+     */
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
+    /**
+     * Increase score 
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
+    }
+        
+    
+    }
+    
+    
+
